e1618094d97048088e85a1e92d437ad2756675cc
DGichuru <dangichuru77@gmail.com>
2018-07-16 12:00:19 +0300

    prototype for full name added

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ function Contact(first, last) {
     this.firstName = first;
     this.lastName = last; 
 }
+Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName
+}
 // Business Logic
 
 $(document).ready(function() {
